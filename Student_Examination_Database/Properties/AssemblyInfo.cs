fbe279b0bcd2cf1ef2b3c3a9e78e5f22f4bf6f66
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-09-15 19:40:16 +0300

    Update AssemblyInfo.cs

--- Student_Examination_Database/Properties/AssemblyInfo.cs
+++ Student_Examination_Database/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Student_Examination_Database")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
